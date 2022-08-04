590d2a559cd1b407498f5b96be7640dc6146be09
vinicius <viniciusventura29@gmail.com>
2022-08-03 21:43:40 -0300

    upgrading the writer page

---
+++ public/js/editor.js
@@ -0,0 +1,45 @@
+const blogTitleField = document.querySelector(".title");
+const articleField = document.querySelector(".article");
+
+// banner
+const bannerImage = document.querySelector("#banner-upload");
+const banner = document.querySelector(".banner");
+let bannerPath;
+
+const publishBtn = document.querySelector(".btn-publish");
+const uploadInput = document.querySelector("#image-upload");
+
+bannerImage.addEventListener("change", () => {
+  uploadImage(bannerImage, "banner");
+});
+
+const uploadImage = (uploadFile, uploadType) => {
+  const [file] = uploadFile.files;
+  if (file && file.type.includes("image")) {
+    const formdata = new FormData();
+    formdata.append("image", file);
+
+    fetch("/upload", {
+      method: "post",
+      body: formdata,
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (uploadType == "image") {
+          addImage(data, file.name);
+        } else {
+          bannerPath = `${location.origin}/${data}`;
+          banner.style.backgroundImage = `url("${bannerPath}")`;
+        }
+      });
+  }
+};
+
+const addImage = (imagepath, alt) => {
+  let curPos = articleField.selectionStart;
+  let textToInsert = `\r![${alt}](${imagepath})\r`;
+  articleField.value =
+    articleField.value.slice(0, curPos) +
+    textToInsert +
+    articleField.value.slice(curPos);
+};
